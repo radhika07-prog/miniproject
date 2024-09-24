64ddf5bbbd258bae07fe05217b31d3bed57514ef
radhika07-prog <radhikadhumal65@gmail.com>
2024-09-24 13:17:25 +0530

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,6 @@
+var memuicon = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+
+memuicon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
